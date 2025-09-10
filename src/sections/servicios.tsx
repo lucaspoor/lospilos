7642e6f1c73da1ea0a676041ed0f33c67e81eb61
hans <hans@welinux.cl>
2025-09-10 13:27:38 -0300

    Agrega sección juegos infantiles

--- src/sections/servicios.tsx
+++ src/sections/servicios.tsx
@@ -4,13 +4,13 @@ const data = [
   {
     title: "AREAS DE CAMPING",
     description:
-"Nuestras parcelas de camping están pensadas para tu comodidad y conexión con la naturaleza. Con espacio para carpas, parrilla y lavadero individual, cada sector ofrece privacidad y comodidad. Aquí podrás disfrutar de noches estrelladas y despertar rodeado de aire puro en un entorno seguro y acogedor.",
+      "Nuestras parcelas de camping están pensadas para tu comodidad y conexión con la naturaleza. Con espacio para carpas, parrilla y lavadero individual, cada sector ofrece privacidad y comodidad. Aquí podrás disfrutar de noches estrelladas y despertar rodeado de aire puro en un entorno seguro y acogedor.",
     img: "/im/nuevas/area.jpeg",
   },
   {
     title: "ACCESO PRIVADO A LA PLAYA",
     description:
-    "Nuestro camping ofrece acceso exclusivo a la playa, ideal para relajarte junto al lago o disfrutar de actividades acuáticas en un entorno seguro y tranquilo. Aquí podrás vivir momentos únicos, lejos de las multitudes, conectándote con la naturaleza en un espacio pensado solo para ti.",
+      "Nuestro camping ofrece acceso exclusivo a la playa, ideal para relajarte junto al lago o disfrutar de actividades acuáticas en un entorno seguro y tranquilo. Aquí podrás vivir momentos únicos, lejos de las multitudes, conectándote con la naturaleza en un espacio pensado solo para ti.",
     img: "/im/ser/playa.jpeg",
   },
   {
@@ -22,39 +22,45 @@ const data = [
   {
     title: "BAÑOS Y ESTACIONAMIENTOS",
     description:
-    "Disfruta de baños y duchas con agua caliente, siempre limpios y cómodos para tu tranquilidad. Además, contamos con amplios estacionamientos seguros y de fácil acceso, pensados para que tu experiencia de camping sea práctica y sin preocupaciones, desde que llegas hasta el momento de partir."
-  ,  img: "/im/nuevas/baños.jpeg",
+      "Disfruta de baños y duchas con agua caliente, siempre limpios y cómodos para tu tranquilidad. Además, contamos con amplios estacionamientos seguros y de fácil acceso, pensados para que tu experiencia de camping sea práctica y sin preocupaciones, desde que llegas hasta el momento de partir.",
+    img: "/im/nuevas/baños.jpeg",
+  },
+  {
+    title: "JUEGOS INFANTILES",
+    description:
+      "Unos clásicos juegos de madera: castillos, casas, toboganes y columpios para que tus hijos puedan desarrollar sus habilidades y compartir con amigos.",
+    img: "/im/gal/juegos-horizontal.jpeg",
   },
 ];
 
 export default function Servicios() {
   return (
     <div className="w-full bg-white flex-col flex items-center  pt-20">
+      <div className="max-w-5xl mx-auto px-6 ">
+        <h2 data-aos="fade-up" className="text-4xl text-cyan-900 mb-4">
+          Instalaciones y Servicios{" "}
+        </h2>
+        <p data-aos="fade-up" className="text-cyan-800">
+          Disfruta de una experiencia única con instalaciones completas y acceso
+          a la naturaleza.
+        </p>
 
-
-
-<div className="max-w-5xl mx-auto px-6 ">
-  <h2 data-aos="fade-up" className="text-4xl text-cyan-900 mb-4">
-Instalaciones y Servicios  </h2>
-  <p data-aos="fade-up" className="text-cyan-800">
-   Disfruta de una experiencia única con instalaciones completas y acceso a la naturaleza.
-  </p>
-
-  <section className="py-16 grid md:grid-cols-2 gap-12">
-    {data.map((item, idx) => (
-      <div data-aos="fade-up" key={idx} className="flex flex-col">
-        <img
-          src={item.img}
-          alt={item.title}
-          className="mb-4 object-cover w-full h-64"
-        />
-        <h3 className="text-xl text-cyan-900 font-bold mb-2">{item.title}</h3>
-        <p className="text-cyan-800">{item.description}</p>
+        <section className="py-16 grid md:grid-cols-2 gap-12">
+          {data.map((item, idx) => (
+            <div data-aos="fade-up" key={idx} className="flex flex-col">
+              <img
+                src={item.img}
+                alt={item.title}
+                className="mb-4 object-cover w-full h-64"
+              />
+              <h3 className="text-xl text-cyan-900 font-bold mb-2">
+                {item.title}
+              </h3>
+              <p className="text-cyan-800">{item.description}</p>
+            </div>
+          ))}
+        </section>
       </div>
-    ))}
-  </section>
-</div>
-
     </div>
   );
 }
